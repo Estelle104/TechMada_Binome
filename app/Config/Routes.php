37e96217d4ba411b5ecc0f8bb3bbc88691e3050e
Andry Fanantenana <andryrandria528@gmail.com>
2026-05-13 11:23:03 +0300

    Employer function

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,6 +10,10 @@ $routes->get('/login', 'Home::login');
 
 $routes->group('employer', ['filter' => 'role:employe'], function($routes) {
     $routes->get('/', 'employer/EmployerController::index');
+    $routes->get('(:num)', 'employer/EmployerController::profile/$1');
+    $routes->post('conges', 'employer/EmployerController::demandeConge');
+    $routes->get('conges', 'employer/EmployerController::listeConge');
+    $routes->post('conges/annuler/(:num)', 'employer/EmployerController::annulerConge/$1');
 });
 
 $routes->group('rh', ['filter' => 'role:rh'], function($routes) {
